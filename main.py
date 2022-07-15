1b3b9d8a1c7791b6c9ac1353321317dcbac922bb
TashaKo12 <belinda.kovalyova@yandex.ru>
2022-07-15 15:23:27 +0300

    Вывела комментарии

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ import requests
 
 from download_image import download_image
 
-url = "https://xkcd.com/1/info.0.json"
 
-response = requests.get(url)
-image_comics_url = response.json()["img"]
-file_name = "com.jpg"
+def get_comics(comics_url):
+    response = requests.get(comics_url)
+    image_comics_url = response.json()["img"]
+    file_name = "com.jpg"
+    comment = response.json()["alt"]
+    print(comment)
+    download_image(image_comics_url, file_name)
 
-download_image(image_comics_url, file_name)
+
+def main():
+    comics_url = "https://xkcd.com/1/info.0.json"
+    get_comics(comics_url)
+
+if __name__ == "__main__":
+    main()
