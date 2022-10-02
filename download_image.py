6f9eb063c006cbbd49f814aee985d213c956ea3d
TashaKo12 <belinda.kovalyova@yandex.ru>
2022-10-02 09:41:16 +0300

    Added documentation

--- download_image.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def download_image(link, file_path, params = None):
-    response = requests.get(link, params = params)
-    response.raise_for_status()
-    with open(file_path, 'wb') as file:
-        file.write(response.content)
--- main.py
+++ main.py
@@ -1,69 +1,150 @@
 import os
+import random
 
 import requests
 from dotenv import load_dotenv
-
-from download_image import download_image
-
-
-def get_image_comics(comics_url):
-    response = requests.get(comics_url)
-    image_comics_url = response.json()["img"]
-    file_name = "com.jpg"
-    comment = response.json()["alt"]
-    download_image(image_comics_url, file_name)
+from urllib.parse import urlparse
 
 
-def check_key(api_url, access_token):
-    api_metod = "groups.get"
-    metod_url = api_url.format(api_metod)
+def download_image(name, img_url):
+    filepath = f"{name}{get_extension(img_url)}"
 
-    params = {
-        "access_token": access_token,
-        "extended": 1,
-        "v": 5.131
-    }
-    response = requests.get(metod_url, params=params)
+    response = requests.get(img_url)
     response.raise_for_status()
-    print(response.json())
+
+    with open(filepath, "wb") as file:
+        file.write(response.content)
+    return filepath
 
 
-def get_addresses_photos(api_url, access_token, client_id):
-    api_metod = "photos.getWallUploadServer"
-    metod_url = api_url.format(api_metod)
-    params = {
-        "access_token": access_token,
-        "group_id": client_id,
-        "v": 5.131
-    }
-    response = requests.get(metod_url, params=params)
+def get_extension(img_url):
+    parsed_url = urlparse(img_url)
+    extension = os.path.splitext(parsed_url.path)[1]
+    return extension
+
+
+def get_comic_info(comic_num):
+    comic_info_url = "https://xkcd.com/{comic}/info.0.json"
+
+    response = requests.get(comic_info_url.format(comic=comic_num))
     response.raise_for_status()
-    return response.json()["response"]["upload_url"]
+    answer = response.json()
+    image = answer["img"]
+    comment = answer["alt"]
+    name = answer["title"]
+    return image, comment, name, comic_num
 
 
-def upload_in_server(dowmload_adress_url, file_name="com.jpg"):
-    with open(file_name, 'rb') as file:
-        files={
+def get_upload_url(token, group_id, version):
+    url = "https://api.vk.com/method/photos.getWallUploadServer"
+    params = {
+        "access_token": token,
+        "v": version,
+        "group_id": group_id
+    }
+
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    answer = response.json()
+    check_response(answer)
+    return answer["response"]["upload_url"]
+
+
+def upload_to_server(token, group_id, version, filename):
+    url = get_upload_url(token, group_id, version)
+    with open(filename, "rb") as file:
+        files = {
             "photo": file
         }
-        response = requests.post(dowmload_adress_url, files=files)
-        response.raise_for_status()
-    
-    print(response.json())
 
+        response = requests.post(url, files=files)
+        response.raise_for_status()
+    answer = response.json()
+    check_response(answer)
+    server = answer["server"]
+    response_hash = answer["hash"]
+    photo = answer["photo"]
+    return server, response_hash, photo
+
+
+def save_photo_to_wall(token, group_id, version, filename):
+    url = "https://api.vk.com/method/photos.saveWallPhoto"
+    server, response_hash, photo = upload_to_server(token, group_id, version, filename)
+
+    params = {
+        "access_token": token,
+        "photo": photo,
+        "v": version,
+        "group_id": group_id,
+        "server": server,
+        "hash": response_hash
+    }
+
+    response = requests.post(url, params=params)
+    response.raise_for_status()
+    answer = response.json()
+    check_response(answer)
+    return answer
+
+
+def publish_to_group(token, group_id, version, filename, comment, comic_num):
+    url = "https://api.vk.com/method/wall.post"
+    saved_photo_info = save_photo_to_wall(token, group_id, version, filename)
+    owner_id = saved_photo_info["response"][0]["owner_id"]
+    media_id = saved_photo_info["response"][0]["id"]
+    attachments = f"photo{owner_id}_{media_id}"
+
+    params = {
+        "access_token": token,
+        "attachments": attachments,
+        "v": version,
+        "owner_id": -int(group_id),
+        "message": comment,
+        "from_group": "1"
+    }
+
+    response = requests.post(url, params=params)
+    response.raise_for_status()
+    check_response(response.json())
+    return f"Комикс №{comic_num} загружен в группу {group_id}."
+
+
+def get_comics_num():
+    last_comic_url = "https://xkcd.com/info.0.json"
+    response = requests.get(last_comic_url)
+    response.raise_for_status()
+    return response.json()["num"]
+
+
+def generate_random_comic():
+    total_comics = get_comics_num()
+    random_comic_num = random.randint(1, total_comics)
+    img_url, comment, title, comic_num = get_comic_info(random_comic_num)
+    return img_url, comment, title, comic_num
+
+
+def check_response(response):
+    if "error" in response:
+        message = response["error"]["error_msg"]
+        error_code = response["error"]["error_code"]
+        raise requests.HTTPError(error_code, message)
 
 
 def main():
     load_dotenv()
-    client_id = os.environ["CLIENT_ID"]
-    access_token = os.environ["ACCESS_TOKEN"]
-    comics_url = "https://xkcd.com/1/info.0.json"
-    api_url = "https://api.vk.com/method/{}"
-    get_image_comics(comics_url)
-    #check_key(api_url, access_token)
-    dowmload_adress_url = get_addresses_photos(api_url, access_token, client_id)
-    upload_in_server(dowmload_adress_url)
-    
+
+    api_version = 5.131
+
+    client_id = os.getenv("VK_CLIENT_ID")
+    access_token = os.getenv("VK_ACCESS_TOKEN")
+    group_id = os.getenv("VK_GROUP_ID")
+
+    try:
+        img_url, author_comment, title, comic_num = generate_random_comic()
+        comic_filepath = download_image(title, img_url)
+        print(publish_to_group(access_token, group_id, api_version, comic_filepath, author_comment, comic_num))
+    finally:
+        os.remove(comic_filepath)
 
 
 if __name__ == "__main__":
